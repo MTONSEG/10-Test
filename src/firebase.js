aecb4e558d11af05ddcc8171053d4f1d27c4283b
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-03-06 23:41:54 +0200

    update firebase config env

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
-import { fbconfig } from '../fb'
+import { initializeApp } from 'firebase/app'
 
 export const firebaseConfig = {
-	apiKey: fbconfig.apiKey,
-	authDomain: fbconfig.authDomain,
-	databaseURL: fbconfig.databaseURL,
-	projectId: fbconfig.projectId,
-	storageBucket: fbconfig.storageBucket,
-	messagingSenderId: fbconfig.messagingSenderId,
-	appId: fbconfig.appId,
-	measurementId: fbconfig.measurementId,
-};
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DB_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESS_SENDER_ID,
+	appId: process.env.REACT_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const analytics = getAnalytics(app);
-const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
